70d0df7aa6fcb98dc1d030a7e9eeb3f540c95b71
ohdam <erfrr@naver.com>
2022-10-19 22:53:04 +0900

    [shader_example]
    -  Shader 클래스 소멸자 작성

--- shader_example/src/shader.h
+++ shader_example/src/shader.h
@@ -6,7 +6,7 @@ class Shader {
 public:
     static ShaderUPtr CreateFromFile(const std::string& filename,GLenum shaderType);
     // 무조건 이 함수를 통해서만 쉐이더를 생성할 수 있도록 한다.
-    ~Shader();
+    ~Shader() {if(m_shader) glDeleteShader(m_shader)};
     uint32_t Get() const { return m_shader; }    
 private:
     Shader() {} // 생성자가 private이기에 외부에서 부를 수 없다. Shader shader X
