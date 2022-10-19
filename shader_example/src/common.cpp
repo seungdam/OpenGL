73c75244c15aec829bac86a89999c85a0b68177e
ohdam <erfrr@naver.com>
2022-10-19 21:59:07 +0900

    [shader_example]
    - 파일 분할 작업 과정에서 발생한 빌드 오류 수정

--- shader_example/src/common.cpp
+++ shader_example/src/common.cpp
@@ -1,6 +1,6 @@
-#include "common.h"
 #include <fstream>
 #include <sstream>
+#include "common.h"
 
 std::optional<std::string> LoadTextFile(const std::string& filename) {
     std::ifstream fin(filename);
@@ -14,22 +14,3 @@ std::optional<std::string> LoadTextFile(const std::string& filename) {
 }
 
 
-void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
-    SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
-    glViewport(0, 0, width, height);
-}
-
-void OnKeyEvent(GLFWwindow* window,
-    int key, int scancode, int action, int mods) {
-    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
-        key, scancode,
-        action == GLFW_PRESS ? "Pressed" :
-        action == GLFW_RELEASE ? "Released" :
-        action == GLFW_REPEAT ? "Repeat" : "Unknown",
-        mods & GLFW_MOD_CONTROL ? "C" : "-",
-        mods & GLFW_MOD_SHIFT ? "S" : "-",
-        mods & GLFW_MOD_ALT ? "A" : "-");
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-    }
-}
