ac2f457daf0e56f8f3ac9243af6f754f0a825fa1
ohdam <erfrr@naver.com>
2022-10-19 22:52:45 +0900

    [shader_example]
    -  간단한 버텍스 쉐이더와 프래그먼트 쉐이더 작성
    - main 에서 해당 쉐이더 파일을 통해 쉐이더 객체 생성

--- shader_example/src/main.cpp
+++ shader_example/src/main.cpp
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "shader.h"
 
 int main(int argc, char** argv) {
     SPDLOG_INFO("Start Program");
@@ -28,6 +29,11 @@ int main(int argc, char** argv) {
     }
     auto glVersion = glGetString(GL_VERSION);
     SPDLOG_INFO("OpenGL context version: {}", glVersion);
+    // shader loading 확인
+    auto vertexShader = Shader::CreateFromFile("./shader/vertex_shader.vs", GL_VERTEX_SHADER);
+    auto fragmentShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    SPDLOG_INFO("vertex shader id: {}", vertexShader->Get());
+    SPDLOG_INFO("fragment shader id: {}", fragmentShader->Get());
     
     OnFramebufferSizeChange(window,WINDOW_WIDTH,WINDOW_HEIGHT);
     glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
